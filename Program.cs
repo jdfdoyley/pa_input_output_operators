964a053d2563c5c4a9b1546900ea7cc37c53d60c
Jason D'Oyley <jdfdoyley@gmail.com>
2022-10-08 18:00:33 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿/*******************************************************************************
+ * Name: Jason D'Oyley
+ * Date: Oct. 7, 2022
+ * Performance Assessment: Input/Output and Operators
+ *
+ * Main application (program) class.
+ * Write an application to demonstrate the use of input, output and operators.
+ * The application must:
+ * - Print a line that states "Your Name - Week 1 PA IO & Operators"
+ * - Demonstrate integer-based operations
+ *    - Retrieve 2 integer values from the user
+ *    - Display the sum of those two values
+ *    - Display the comparison results of value 1 to value 2, including:
+ *      - Equality - whether the values are equal or not equal
+ *      - Size - whether value 1 is greater than or less than value 2
+ *      - Size with equality - whether value 1 is greater than or equal to or
+ *        less than or equal to value 2
+ * - Demonstrate floating point-based operations
+ * - Retrieve 2 floating point values from the user
+ * - Display floating point values to 4-decimal point precision
+ * - Display the sum of those two values
+ * - Display the comparison results of value 1 to value 2, including:
+ *    - Equality - whether the values are equal or not equal
+ *    - Size - whether value 1 is greater than or less than value 2
+ *    - Size with equality - whether value 1 is greater than or equal to or
+ *      less than or equal to value 2
+ */
+
+public class InputOutputOperator
+{
+    static void Main(string[] args)
+    {
+        // Code here ...
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# Input/Output and Operators
+
+Write an application to demonstrate the use of input, output and operators.
+The application must:
+
+- Print a line that states "Your Name - Week 1 PA IO & Operators"
+- Demonstrate integer-based operations
+  - Retrieve 2 integer values from the user
+  - Display the sum of those two values
+  - Display the comparison results of value 1 to value 2, including:
+    - Equality - whether the values are equal or not equal
+    - Size - whether value 1 is greater than or less than value 2
+    - Size with equality - whether value 1 is greater than or equal to or less than or equal to value 2
+- Demonstrate floating point-based operations
+- Retrieve 2 floating point values from the user
+- Display floating point values to 4-decimal point precision
+- Display the sum of those two values
+- Display the comparison results of value 1 to value 2, including:
+  - Equality - whether the values are equal or not equal
+  - Size - whether value 1 is greater than or less than value 2
+  - Size with equality - whether value 1 is greater than or equal to or less than or equal to value 2
